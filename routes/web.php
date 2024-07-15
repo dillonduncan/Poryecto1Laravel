07a88761083fde0ac3042f261942e3e52ffaf153
Dillon Duncan <dillonduncan0607@gmail.com>
2024-07-15 16:25:35 -0500

    rutas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
+//el orden de las rutas importa
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/posts', function () {
+    return 'vista de posts';
+});
+
+Route::get('/posts/create', function(){
+    return "Aqui se mostrara la vista de crear";
+});
+
+Route::get('/posts/{post}', function ($post) {
+    return "vista de {$post}";
+});
+
+Route::get('/posts/{post}/{categoria?}', function ($post,$categoria=null) {
+    if($categoria){
+        return "vista de {$post} de la categoria {$categoria}"; 
+    }
+    return "vista de {$post}";
 });
