40000ee5227a47113a6d3c12e7e8d58ac7a04d64
Dillon Duncan <dillonduncan0607@gmail.com>
2024-07-21 18:09:53 -0500

    uso de yield y stack

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,18 @@
 @extends('layouts.app')|
 
+@section('titulo','Principal')
+@push('css')
+    <style>
+        body{
+            font-family: 'Times New Roman', Times, serif;
+        }
+    </style>
+@endpush
 @section('contenido')
     <div class="max-w-4xl mx-auto px-4">
         <h1>Bienvenido la vista principal</h1>
 
-        <x-alert2 type="danger" class="mb-4">
+        <x-alert2 type="info" class="mb-4">
             <x-slot name="title">
                 contenido de la alerta
             </x-slot>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>@yield('titulo','Home')</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    @stack('css')
 </head>
-
 <body>
     <header></header>
     @yield('contenido')
     <footer></footer>
 </body>
-
 </html>
