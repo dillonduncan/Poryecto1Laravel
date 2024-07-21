1a2aa9f447f24e9f557b4d58133e4982b71b3f72
Dillon Duncan <dillonduncan0607@gmail.com>
2024-07-21 16:49:57 -0500

    componente alert

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-    <h1>Bienvenido a la vista principal</h1>
+    <div class="max-w-4xl mx-auto px-4">
+        <h1>Bienvenido la vista principal</h1>
+
+        <x-alert type="success" class="mb-4">
+            <x-slot name="title">
+                contenido de la alerta
+            </x-slot>
+            Bienvenido la vista principal
+        </x-alert>
+        <p>habla parce</p>
+    </div>
 </body>
+
 </html>
